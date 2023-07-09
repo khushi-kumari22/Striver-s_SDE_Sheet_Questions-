6c4aeb0b1a84e9f92e27a546152edabebbd07187
Khushi Kumari <119166889+khushi-kumari22@users.noreply.github.com>
2023-07-10 00:01:37 +0530

    Add files via upload
    Added

---
+++ Strivers SDE Sheet/Arrays/majorityElement.java
@@ -0,0 +1,24 @@
+//  Q:  Majority Element
+
+class Solution {
+    public int majorityElement(int[] nums) {
+
+        int n = nums.length;
+
+        for (int i = 0; i < n; i++) {
+            int count = 0;
+
+            for (int j = 0; j < n; j++) {
+                if (nums[j] == nums[i]) {
+                    count++;
+                }
+            }
+            if (count > (n / 2))
+                return nums[i];
+        }
+        return -1;
+    }
+    //   Best solution:
+    // Arrays.sort(nums);
+   //  return nums(nums.length/2);
+}
