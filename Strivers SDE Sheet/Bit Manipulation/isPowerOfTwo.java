d1086fc95a7202e06e84c915bdf988b9f9ee35c7
Khushi Kumari <119166889+khushi-kumari22@users.noreply.github.com>
2023-07-18 23:46:52 +0530

    Add files via upload
    Added

---
+++ Strivers SDE Sheet/Bit Manipulation/isPowerOfTwo.java
@@ -0,0 +1,13 @@
+//  Q: Check if a number is power of 2 or not
+
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        if(n <= 0){
+            return false;
+        }
+        if((n & (n-1)) == 0){
+            return true;
+        }
+        return false;
+    }
+}
