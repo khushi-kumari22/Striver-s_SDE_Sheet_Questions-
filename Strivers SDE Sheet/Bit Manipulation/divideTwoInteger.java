49f1c90b84b2dcddcc4434d647027e6455046bca
Khushi Kumari <119166889+khushi-kumari22@users.noreply.github.com>
2023-07-21 23:22:05 +0530

    Add files via upload
    added

--- Strivers SDE Sheet/Bit Manipulation/divideTwoInteger.java
+++ Strivers SDE Sheet/Bit Manipulation/divideTwoInteger.java
@@ -1,27 +1,28 @@
-//  Q: Divide two integers without using multiplication, division and modulous
+//  Q:  Divide Two Integers
+
 
 public class Solution{
     public static int divideTwoInteger(int dividend, int divisor) {
         // Write your code here.
 
-        int quotient = 0;
+         int quotient = 0;
 
-        boolean isDividendNegative = false;
-        if(dividend < 0){
-            dividend = dividend*(-1);
-            isDividendNegative = true;
-        }
+         boolean isDividendNegative = false;
+         if(dividend < 0){
+             dividend = dividend*(-1);
+             isDividendNegative = true;
+         }
 
-        boolean isDivisorNegative = false;
-        if(divisor < 0){
-            divisor = divisor*(-1);
-            isDivisorNegative = true;
-        }
+         boolean isDivisorNegative = false;
+         if(divisor < 0){
+              divisor = divisor*(-1);
+              isDivisorNegative = true;
+         }
 
-        while(dividend >= divisor){
-            dividend = dividend - divisor;
-            quotient = quotient + 1;
-        }
+         while(dividend >= divisor){
+             dividend = dividend - divisor;
+             quotient = quotient + 1;
+             }
 
         // Both are negative.
         if(isDividendNegative && isDivisorNegative)
@@ -35,7 +36,7 @@ public class Solution{
             return quotient;
         }
 
-        return (-1)*quotient;
-    }
-}
+         return (-1)*quotient;
+     }
+ }
  
