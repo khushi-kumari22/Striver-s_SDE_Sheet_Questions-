b00e5ec028e70cd1694d3027e221f84a4494e55d
Khushi Kumari <119166889+khushi-kumari22@users.noreply.github.com>
2023-08-09 23:03:25 +0530

    Add files via upload

---
+++ Strivers SDE Sheet/Sorting/findDisappearedNumbers.java
@@ -0,0 +1,26 @@
+//  Q: Find all numbers disappeared in an array
+
+class Solution {
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        ArrayList<Integer> ans = new ArrayList<>();
+
+        int i = 0;
+        while(i<nums.length){
+            int correct = nums[i] - 1;
+            if(nums[i]!=nums[correct]){
+                int temp = nums[i];
+                nums[i] = nums[correct];
+                nums[correct] = temp;
+            }
+            else{
+                i++;
+            }
+        }
+        for( i=0; i<nums.length; i++){
+            if(nums[i]!=i+1){
+                ans.add(i+1);
+            }
+        }
+        return ans;
+    }
+}
