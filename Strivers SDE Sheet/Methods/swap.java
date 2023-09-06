1a750df6a7e18b01546ae9eb221bd6fff650cef2
Khushi Kumari <119166889+khushi-kumari22@users.noreply.github.com>
2023-09-07 00:49:02 +0530

    Add files via upload

---
+++ Strivers SDE Sheet/Methods/swap.java
@@ -0,0 +1,24 @@
+public class swap {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        swap(a, b);
+        System.out.println(a + " " + b);
+    }
+
+    static void swap(int a ,int b){
+        int temp = a;
+        a = b;
+        b = temp;
+
+    }
+}
+ /*   int a = 10;
+    int b = 20;
+
+    int temp = a;
+    a = b;
+    b = temp;
+    System.out.println(a + " " + b);
+     */
+
