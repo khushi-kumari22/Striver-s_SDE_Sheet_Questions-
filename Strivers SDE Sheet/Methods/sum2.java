86ae4f6bcae5ca6ecb741cef6779e41e0e381fe3
Khushi Kumari <119166889+khushi-kumari22@users.noreply.github.com>
2023-09-07 00:49:24 +0530

    Add files via upload

---
+++ Strivers SDE Sheet/Methods/sum2.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class sum2 {
+    public static void main(String[] args) {
+//        int ans= sum2();
+//        System.out.println(ans);
+//
+//    }
+//
+//      static int sum2(){
+//       Scanner in = new Scanner(System.in);
+//        System.out.println("enter no 1:");
+//        int num1 = in.nextInt();
+//        System.out.println("enter no 2:");
+//        int num2 = in.nextInt();
+//       int sum2 = num1 + num2;
+//       return(sum2);
+
+        int ans = summ2(20, 30);
+        System.out.println(ans);
+    }
+
+    static int summ2(int a, int b) {
+        int sum2 = a + b;
+        return sum2;
+
+    }
+}
