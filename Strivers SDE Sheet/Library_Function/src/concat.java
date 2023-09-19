76faffd6735aa381fefd999484c51339714e6f93
Khushi Kumari <119166889+khushi-kumari22@users.noreply.github.com>
2023-09-19 23:42:22 +0530

    Add files via upload

---
+++ Strivers SDE Sheet/Library_Function/src/concat.java
@@ -0,0 +1,8 @@
+public class concat {
+    public static void main(String[] args) {
+        String str1 = "Java";
+        String str2 = "Programming";
+
+        System.out.println(str1.concat(str2));
+    }
+}
