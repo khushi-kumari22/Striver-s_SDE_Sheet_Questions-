09ffeeb3853e1f528f0a985b0e80baeccabeb53d
Khushi Kumari <119166889+khushi-kumari22@users.noreply.github.com>
2023-09-20 01:51:39 +0530

    Add files via upload

---
+++ Strivers SDE Sheet/Library_Function/src/equals.java
@@ -0,0 +1,8 @@
+public class equals {
+    public static void main(String[] args) {
+        String  str1 = "Khushi";
+        String  str2 = "Khushi";
+
+        System.out.println(str1.equals(str2));
+    }
+}
