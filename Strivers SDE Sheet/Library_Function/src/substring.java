9f46822c2a237e477f88c3de39849a5559da4705
Khushi Kumari <119166889+khushi-kumari22@users.noreply.github.com>
2023-09-19 23:41:21 +0530

    Add files via upload

---
+++ Strivers SDE Sheet/Library_Function/src/substring.java
@@ -0,0 +1,19 @@
+public class substring {
+    public static void main(String[] args) {
+        String str1 = "Program";
+
+        //  1st character to the last character
+        System.out.println(str1.substring(0));  // program
+
+        // 4th character to the last character
+        System.out.println(str1.substring(3));  // gram
+
+        // 1st to the 7th  character
+        System.out.println(str1.substring(0, 7));  // program
+
+        // 1st to the 5th  character
+        System.out.println(str1.substring(0, 5));  // progr
+
+
+    }
+}
