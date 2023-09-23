8399071db17cd60d8977214a1a086def3ab94182
Khushi Kumari <khushi0884@gmail.com>
2023-09-23 23:58:49 +0530

    Add files via upload

---
+++ Strivers SDE Sheet/Library_Function/src/toCharArray.java
@@ -0,0 +1,10 @@
+public class toCharArray {
+    public static void main(String[] args) {
+        String str1 = "Java Programming";
+
+        char[] result ;
+        result = str1.toCharArray();
+        System.out.println(result); // Java Programming
+
+    }
+}
