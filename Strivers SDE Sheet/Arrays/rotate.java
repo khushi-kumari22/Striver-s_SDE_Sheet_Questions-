30a42df276c2ceadae785ea472e0012cd8318d9d
Khushi Kumari <khushi0884@gmail.com>
2023-10-07 04:03:53 +0530

    Add files via upload

--- Strivers SDE Sheet/Arrays/rotate.java
+++ Strivers SDE Sheet/Arrays/rotate.java
@@ -1,20 +1,25 @@
-//  Q: Left Rotate the Array by One
+public class rotate {
+        void reverse(int[] nums, int start, int end){
 
-class Solution {
-    public void rotate(int[] nums, int k) {
-        k = k % nums.length;
-        swap(nums, 0, nums.length - 1);
-        swap(nums, 0, k - 1);
-        swap(nums, k, nums.length - 1);
-    }
-    static void swap(int[]nums, int start, int end){
-        while(start<end){
-            int temp = nums[start];
-            nums[start] = nums[end];
-            nums[end] = temp;
+            while(start<=end){
+                int temp = nums[start];
+                nums[start] = nums[end];
+                nums[end] = temp;
 
-            start++;
-            end--;
+                start++;
+                end--;
+
+            }
         }
+        public void rotate(int[] nums, int k) {
+
+            int n = nums.length;
+            if(k>n){
+                k=k%n;
+            }
+            reverse(nums, 0, n-1);
+            reverse(nums, 0, k-1);
+            reverse(nums, k,n-1);
+        }
+
     }
-}
